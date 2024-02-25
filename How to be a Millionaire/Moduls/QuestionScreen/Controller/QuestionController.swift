5ee65fbe0a01faadeea263fe2e2e092baf29ee93
Nikita Kornuk <kornyk62@gmail.com>
2024-02-25 19:15:51 +0100

    add for the screen question

--- How to be a Millionaire/Moduls/QuestionScreen/Controller/QuestionController.swift
+++ How to be a Millionaire/Moduls/QuestionScreen/Controller/QuestionController.swift
@@ -5,4 +5,13 @@
 //  Created by NikitaKorniuk   on 25.02.2024.
 //
 
-import Foundation
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
